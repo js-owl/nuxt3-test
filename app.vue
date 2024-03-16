c4f1e35fdcb456d372f6bfbb0aad254dbc28b3dd
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-16 07:41:53 +0300

    Add my-profile page

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <div>hi</div>
+    <NuxtLayout><NuxtPage /></NuxtLayout>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,6 @@ import { resolve } from 'path';
 export default defineNuxtConfig({
   alias: { '@': resolve(__dirname, '/') },
   devtools: { enabled: true },
-  css: ['~/assets/main.scss']
+  css: ['~/assets/main.scss'],
+  postcss: { plugins: { tailwindcss: {}, autoprefixer: {} } }
 });
