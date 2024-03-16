e99147dc2186ab8eca6e06fee9e17cb373a044ca
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-16 23:13:13 +0300

    Add useAsyncData

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="Main container py-3 mx-auto">
-    <p>{{ pending ? 'Loading...' : products }}</p>
-    <Counter />
+    <p>{{ pending ? 'Loading...' : productCount }}</p>
+    <button @click="refresh">Refresh</button>
+    <!-- <p>{{ pending ? 'Loading...' : products }}</p> -->
+    <!-- <Counter />
     <Counter />
     <ContentDoc />
     <div style="display: flex">
@@ -10,20 +12,25 @@
     </div>
     <img src="/2.png" alt="aaa" />
     <Alert />
-    <ProfileHeaderAvatar />
+    <ProfileHeaderAvatar />-->
   </div>
 </template>
 <script setup>
+const { data: productCount, pending } = await useAsyncData('count', () =>
+  $fetch('/api/count')
+);
+const refresh = () => refreshNuxtData('count');
 // const { data: products, pending } = await useFetch('/api/products', {
 //   transform: (_products) => _products.data
 // });
-const { data: products, pending } = await useLazyFetch('/api/products');
 
-import { useCounterStore } from '~/stores/myStore';
-const store = useCounterStore();
-const counter = useState('counter', () => Math.round(Math.random() * 1000));
-const { say } = useUtils();
-say();
-const { $hello } = useNuxtApp();
-$hello('plugins');
+// const { data: products, pending } = await useLazyFetch('/api/products');
+
+// import { useCounterStore } from '~/stores/myStore';
+// const store = useCounterStore();
+// const counter = useState('counter', () => Math.round(Math.random() * 1000));
+// const { say } = useUtils();
+// say();
+// const { $hello } = useNuxtApp();
+// $hello('plugins');
 </script>
