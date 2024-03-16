612842936702a9c3874c434efa30b867efbdfcb5
Климовских Александр Викторович <av.klimovskih@prof-itgroup.ru>
2024-03-16 15:24:35 +0300

    Add server api/hello

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="Main container py-3 mx-auto">
+    <pre>{{ data.name }}</pre>
     <Counter />
     <Counter />
     <ContentDoc />
@@ -13,6 +14,7 @@
   </div>
 </template>
 <script setup>
+const { data } = await useFetch('/api/hello');
 import { useCounterStore } from '~/stores/myStore';
 const store = useCounterStore();
 const counter = useState('counter', () => Math.round(Math.random() * 1000));
